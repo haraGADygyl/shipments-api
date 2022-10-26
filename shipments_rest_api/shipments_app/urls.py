129b109347a23f40efc2eca8ecf9c1aef607b39e
Tihomir Manushev <ti60gtt@gmail.com>
2022-10-26 19:57:13 +0300

    Removed old version

--- shipments_rest_api/shipments_app/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from shipments_app.views import ShipmentModelViewSet
-
-RESOURCE_SHIPMENTS = r'shipments'
-
-router = routers.DefaultRouter()
-router.register(RESOURCE_SHIPMENTS, ShipmentModelViewSet, basename=RESOURCE_SHIPMENTS)
-urlpatterns = router.urls
